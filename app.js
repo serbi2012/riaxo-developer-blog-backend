bf73aa26d71bfd5cf52c518a927fcda8760177db
serbi2012 <serbi2012@naver.com>
2023-11-22 15:09:38 +0900

    ♻️ Refactor: 불필요한 의존성 제거 및 코드 리팩토링

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const path = require("path");
 const logger = require("morgan");
 const cors = require("cors");
 
